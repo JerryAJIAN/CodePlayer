fe1af0c5efa0edc947518106c74e0648d6376c86
JerryAJIAN <2016928619@qq.com>
2022-04-21 14:24:18 +0800

    更新res

--- app/src/main/java/com/example/codeplayer/MainActivity.java
+++ app/src/main/java/com/example/codeplayer/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.codeplayer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+/**
+ * @author ASUS_JAJIAN
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/example/codeplayer/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.codeplayer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ * @author ASUS_JAJIAN
+ */
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                //去标题
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+                setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(this,PlayService.class);
+        startService(intent);
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },3000);
+    }
+
+}
